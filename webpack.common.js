35d0748c8d5753270632b624ba326f0e0f63f31c
btran <nguyenbao1003@outlook.com>
2023-03-25 20:53:29 -0400

    WIP

--- webpack.common.js
+++ webpack.common.js
@@ -2,9 +2,11 @@ const { CleanWebpackPlugin } = require('clean-webpack-plugin');
 const ESLintPlugin = require('eslint-webpack-plugin');
 const HtmlWebpackPlugin = require('html-webpack-plugin');
 const MiniCssExtractPlugin = require('mini-css-extract-plugin');
+const path = require('path');
 
 module.exports = {
   entry: './src/main.tsx',
+  context: path.resolve(__dirname),
   module: {
     rules: [
       {
